1607081547918ce3ecbd0388db6ea41b2be3bd7b
MarcioGabrieldeOliveira <marciogabrieldeoliveira@gmail.com>
2022-03-24 11:47:31 -0300

    projeto site versao do angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarrinhoComponent } from './views/carrinho/carrinho.component';
+import { FaleConoscoComponent } from './views/fale-conosco/fale-conosco.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegistroComponent } from './views/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "fale-conosco",  component: FaleConoscoComponent},
+  { path: "login", component: LoginComponent},
+  { path: "registro", component: RegistroComponent},
+  { path: "carrinho", component: CarrinhoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { FaleConoscoComponent } from './views/fale-conosco/fale-conosco.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegistroComponent } from './views/registro/registro.component';
+import { CarrinhoComponent } from './views/carrinho/carrinho.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FaleConoscoComponent,
+    LoginComponent,
+    RegistroComponent,
+    CarrinhoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
