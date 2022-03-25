1bbfd8a28b974ff0fb0572fd537b2ea1a4742a23
MarcioGabrieldeOliveira <marciogabrieldeoliveira@gmail.com>
2022-03-25 01:10:05 -0300

    Implementação da Blacklist SQL Injection e configurações de erro

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,37 @@ import { User } from 'src/app/models/user';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
   loginModel = new User();
 
+  mensagemErro = ""
+
   onSubmit() {
-    console.log(this.loginModel)
+  console.log(this.loginModel)
+
+  let erroEncontrado = 0;
+    
+  const listaPalavras: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "insert ", "exec ", "\"", "\'", "--", "#", "*", ";"]
+    
+  listaPalavras.forEach(palavra => {
+      if(this.loginModel.email?.toLowerCase().includes(palavra)){
+        this.mensagemErro = "Dados Inválidos: " + palavra;
+
+        erroEncontrado = 1;
+      }
+    })
+    
+
+    if(erroEncontrado == 0) {
+    this.loginService.login(this.loginModel).subscribe( (response) => {
+      this.router.navigateByUrl('/carrinho')
+      this.mensagemErro = "Sucesso"
+    }, (respostaErro) =>{
+      this.mensagemErro = respostaErro.error;
+    })
   }
+}
 }
